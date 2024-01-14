293cc0e7f95e726f5cceb07b3f1796fcb1f87974
subodh2604 <53229843+subodh2604@users.noreply.github.com>
2024-01-14 20:43:38 +0530

    committing main java file

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public  class Main{
+    public static void main(String args[])
+    {
+       System.out.println("This is java program from jenkins...."); 
+       System.out.println("hello"); 
+       System.out.println(new Date()); 
+    }
+}
